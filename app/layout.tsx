1b8ee2a00e680108a760c709eda21bceb4474858
thaiducpro <nguyenhuuthai.pro@gmail.com>
2025-04-26 23:49:08 +1000

    save sate

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
       <body>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
