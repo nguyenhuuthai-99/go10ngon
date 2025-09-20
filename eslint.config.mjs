556269e1441899b9cbc9e74478e145502a0dcc75
thaiducpro <nguyenhuuthai.pro@gmail.com>
2025-09-20 18:20:00 +0930

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,37 @@
+// eslint.config.js
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import nextPlugin from "@next/eslint-plugin-next";
 import { defineConfig } from "eslint/config";
-import * as compat from "typescript-eslint";
 
-export default defineConfig(
-  [
-    {
-      files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-      plugins: { js },
-      extends: ["js/recommended"],
-    },
-    {
-      files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-      languageOptions: { globals: globals.browser },
-    },
-    tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
-  ],
-  compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
     rules: {
+      ...js.configs.recommended.rules,
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    ...tseslint.configs.recommended[0], // TypeScript rules
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    ...pluginReact.configs.flat.recommended, // React rules
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: { "@next/next": nextPlugin },
+    rules: {
+      ...nextPlugin.configs["core-web-vitals"].rules,
       "@typescript-eslint/no-unused-vars": "off",
     },
-  }),
-);
+  },
+]);
